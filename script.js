7b53f57ceb30664e3264beb6ae2cc062156e7286
AmanDhungel <80142075+AmanDhungel@users.noreply.github.com>
2024-01-13 12:13:35 +0545

    commit changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 /**
  * Weather App
- */
+*/
 
 // API_KEY for maps api
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 /**
  * Retrieve weather data from openweathermap
- */
+*/
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
@@ -17,9 +17,6 @@ getWeatherData = (city) => {
   })
 }
 
-/**
- * Retrieve city input and get the weather data
- */
 searchCity = () => {
   const city = document.getElementById('city-input').value;
   getWeatherData(city)
@@ -31,15 +28,17 @@ searchCity = () => {
   })
 }
 
+
+
 /**
  * Show the weather data in HTML
  */
 showWeatherData = (weatherData) => {
   document.getElementById("city-name").innerText = weatherData.name;
   document.getElementById("weather-type").innerText = weatherData.weather[0].main;
-  document.getElementById("temp").innerText = weatherData.main.temp;
-  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
-  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+  document.getElementById("temp").innerText = weatherData.main.temp + "F";
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min + "F";
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max + "F";
   document.getElementById("weather-output").classList.add('visible');
 }
 
